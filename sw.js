b255768afdb889c376e714b57735aee9cc04e941
Neeraj <neerajdana9@gmail.com>
2020-10-29 13:40:22 +0530

    Updates

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log("cacheing the assets");
-      cache.addAll(assets);
+      cache.addAll(assets).then((d) => console.loh("done caching", d));
     })
   );
 });
@@ -18,9 +18,14 @@ self.addEventListener("activate", (event) => {
 });
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
-    })
+    caches
+      .match(event.request)
+      .then((res) => {
+        return res || fetch(event.request);
+      })
+      .catch((err) => {
+        console.log("in fetch", err);
+      })
   );
   console.log("event", event);
 });
