b35fec19f444a64f6202c8fc84abec4c17c7e0c9
Neeraj <neerajdana9@gmail.com>
2020-10-29 13:49:51 +0530

    change for qa branch

--- public/sw.js
+++ public/sw.js
@@ -8,7 +8,13 @@ self.addEventListener("install", (event) => {
   console.log("service worker installed");
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
-      cache.addAll(assets);
+      console.log("cacheing the assets");
+      cache
+        .addAll(assets)
+        .then((d) => console.loh("done caching", d))
+        .catch((err) => {
+          console.log("in addAll", err);
+        });
     })
   );
 });
@@ -16,5 +22,15 @@ self.addEventListener("activate", (event) => {
   console.log("service worker activated");
 });
 self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((res) => {
+        return res || fetch(event.request);
+      })
+      .catch((err) => {
+        // console.log("in fetch", err);
+      })
+  );
   console.log("event", event);
 });
