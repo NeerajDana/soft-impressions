4f6c99e873f1a975bfa937b1c86dd01166394aee
Neeraj <neerajdana9@gmail.com>
2020-10-29 11:45:09 +0530

    Initialize project using Create React App

---
+++ const.ts
@@ -0,0 +1,3 @@
+export const privateKey = "DT1x-jZI1FFgdsFFRlQNBGihf7NuYOS93MTAHNh2TUw";
+export const publicKey =
+  "BGjXeKsGiu3BWngrAH9KmDXIcUpSMNKR7mkoUWk-LAa58f3ElAa_3xiNZutcWSqW1fmm4KVOEbjR0wepX0taBbM";
